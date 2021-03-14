8e93c63fe18dc1ff42f5146bdb53cf71fc09ec64
kianniek <56929026+kianniek@users.noreply.github.com>
2021-03-14 13:22:17 +0100

    Update loader.js

--- Pages-NL/assets/js/loader.js
+++ Pages-NL/assets/js/loader.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelector('a');
+    const anchors = document.querySelectorAll('a');
     setTimeout(() => {
         transition_el.classList.remove('is-active');
     }, 500);
